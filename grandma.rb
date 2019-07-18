1163ec997c66bb9592d2a3140972735f96cc9163
Stef Spears <spears.sj87@gmail.com>
2019-07-18 01:12:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,5 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else return "HUH?! SPEAK UP, SONNY!"
+  
 end
